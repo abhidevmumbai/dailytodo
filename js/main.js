5740615a386dad22d276d6efd70e95495b7c696b
Abhishek Dewadiga <adewadiga@mum-1adewadi-m.local>
2014-06-01 13:38:49 +0530

    Added add task functionality

--- js/main.js
+++ js/main.js
@@ -3,16 +3,41 @@ var toDo = {
 	week: [],
 	today: null,
 	tomorrow: null,
+	todoObj: {},
 	init: function () {
 		this.wrapperEl = $('#wrapper');
-		this.getCurrentWeek();
 		this.renderDays();
+		this.bindEvents();
+	},
 
+	bindEvents: function () {
+		// Add task btn
+		$('.weekday').on('click', '.addTaskBtn', function () {
+			var currDate = $(this).parent().data('date'),
+				task = $('<li/>').attr('contenteditable','true');
+			
+			$(this).siblings('ul').append(task);
+		});
+
+
+		$('.weekday ul').on('focusout', 'li', function (index) {
+			var task = $(this).text(),
+				currDate = $(this).parent().parent().data('date');
+			if (task == '') {
+				$(this).remove();
+			} else {
+				toDo.addTask(task, currDate);
+			}
+			console.log(task + ' - ' + currDate);
+		});
 	},
 
 	renderDays: function () {
 		var htmlStr = '',
 			hlClass = '';
+
+		// Get the current week
+		this.getCurrentWeek();
 
 		for (var i = 0; i < 7; i++) {
 			if (this.today.getDate() == this.week[i].getDate()) {
@@ -28,9 +53,11 @@ var toDo = {
 								+ this.week[i].getDayName() 
 							+ '</span>'
 							+ '<span class="date FR">'
-								+ this.week[i].getMonthName()+ ' ' +this.week[i].getFullYear()
+								+ this.week[i].getDate() + ' ' + this.week[i].getMonthName()+ ' ' +this.week[i].getFullYear()
 							+ '</span>'
 		    			+ '</div>'
+		    			+ '<ul>'
+						+ '</ul>'
 		    			+ '<span class="addTaskBtn">+ click to add task</span>'
 		    		+
 		    		'</div>';
@@ -38,9 +65,12 @@ var toDo = {
 		this.wrapperEl.html(htmlStr);
 	},
 
-	renderHtml: function () {
+	renderTasks: function () {
+		for (var i = 0; i < 7; i++) {
 
+		}
 	},
+
 
 	getCurrentWeek: function () {
 		this.today = new Date();
@@ -56,6 +86,23 @@ var toDo = {
 				day = start.setDate(start.getDate() + 1);
 			}
 			// console.log(this.week);
+	},
+
+	addTask: function (task, currDate) {
+		var count = $('.weekday[data-date="'+ currDate +'"] li').length,
+			id = 0;
+		if (this.todoObj[currDate] == undefined) {
+			this.todoObj[currDate] = [];
+		}
+		if (count == 1) {
+			id = 0;
+		} else {
+			id = --count;
+		}
+		console.log(id);
+		this.todoObj[currDate][id] = task;
+		// console.log(currDate);
+		localStorage.setItem('todo', JSON.stringify(this.todoObj));
 	}
 }
 
