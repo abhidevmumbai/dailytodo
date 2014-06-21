0ee28970cf0ef27cd2de0184ea3383a695d0f000
Abhidev <abhidevmumbai@gmail.com>
2014-06-21 19:37:11 +0530

    Added a util function to select text inside a dom element

--- js/utils.js
+++ js/utils.js
@@ -16,3 +16,20 @@ Date.prototype.getMonthName = function() {
 Date.prototype.getDayName = function() {
     return this.dayNames[this.getDay()];
 };
+
+var utils = {
+	// Selects text within a dom element
+	select_all: function (el) {
+        if (typeof window.getSelection != "undefined" && typeof document.createRange != "undefined") {
+            var range = document.createRange();
+            range.selectNodeContents(el);
+            var sel = window.getSelection();
+            sel.removeAllRanges();
+            sel.addRange(range);
+        } else if (typeof document.selection != "undefined" && typeof document.body.createTextRange != "undefined") {
+            var textRange = document.body.createTextRange();
+            textRange.moveToElementText(el);
+            textRange.select();
+        }
+    }
+}
